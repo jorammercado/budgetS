e70717318474151c77ecba3d5edba5360e44b1a4
arim83 <jorammercado@pursuit.org>
2023-10-17 09:43:05 -0400

    model added

---
+++ models/transaction.js
@@ -0,0 +1,66 @@
+module.exports = [
+    {
+      item_name: "income jan",
+      amount: 500,
+      date: "1-3-2023",
+      from: "employer",
+      category: "income",
+      inOrOut: true
+    },
+    {
+      item_name: "savings",
+      amount: -200,
+      date: "1-29-2023",
+      from: "wife",
+      category: "savings",
+      inOrOut: false
+    },
+    {
+      item_name: "cat food",
+      amount: -10,
+      date: "2-7-2023",
+      from: "pet store",
+      category: "pet food",
+      inOrOut: false
+    },
+    {
+      item_name: "car repair",
+      amount: -200,
+      date: "2-28-2023",
+      from: "car shop",
+      category: "car stuff",
+      inOrOut: false
+    },
+    {
+      item_name: "income march",
+      amount: 1000,
+      date: "3-8-2023",
+      from: "employer",
+      category: "income",
+      inOrOut: true
+    },
+    {
+      item_name: "doctor visit",
+      amount: -300,
+      date: "4-20-2023",
+      from: "hospital",
+      category: "health",
+      inOrOut: false
+    },
+    {
+      item_name: "groceries",
+      amount: -10,
+      date: "5-7-2023",
+      from: "wall-mart",
+      category: "subsistence living",
+      inOrOut: false
+    },
+    {
+      item_name: "new phone",
+      amount: -400,
+      date: "6-9-2023",
+      from: "apple",
+      category: "luxury",
+      inOrOut: false
+    }
+  ]
